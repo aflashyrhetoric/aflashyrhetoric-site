b241f70b8b0023139e130a45d2eb7ed5e8c47f4e
aflashyrhetoric <aflashyrhetoric@gmail.com>
2015-01-12 12:22:04 -0500

    Ran migrations on new database (postgres)

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150107222043) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admin_users", force: :cascade do |t|
     t.string   "first_name",      limit: 25
     t.string   "last_name",       limit: 50
